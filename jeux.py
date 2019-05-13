043afca3b3ec3a08e3c3bf8f348ab2039b8665fe
marine bijon <marine.b.58@hotmail.fr>
2019-05-13 17:48:01 +0200

    jeux.

---
+++ jeux.py
@@ -0,0 +1,39 @@
+import random
+
+borne1 = input("Saisissez un nombre qui sera la plus petite borne :")
+while True :
+    try :
+        borne1 = int(borne1)
+        break
+    except :
+        borne1 = input("Saisissez un nombre qui sera la plus petite borne :")
+
+borne2 = input("Saisissez un nombre qui sera la plus grande borne :")
+while True :
+    try :
+        borne2 = int(borne2)
+        if borne1 >= borne2 :
+            print("Saisissez un nombre plus grand que : ", borne1)
+            borne2 = input()
+        else :
+            break
+    except :
+        borne2 = input("Saisissez un nombre qui sera la plus grande borne :")
+
+nombre = random.randint(borne1, borne2)
+while True:
+    print("Saisissez un nombre entre ", borne1, " et ", borne2, " :")
+    saisi = input()
+    try :
+        saisi = int(saisi)
+    except:
+        saisi = input("Saisissez un nombre :")
+    if saisi == nombre :
+        print("c'est gagné")
+        break
+    elif saisi > nombre :
+        print("trop grand")
+        borne2 = borne2 - 1
+    else :
+        print("trop petit")
+        borne1 = borne1 + 1
